f910bf8e4f9220fe832f86c6f0f9b6e84e6b7dfb
divingbeetle <radi_ohead@yonsei.ac.kr>
2024-08-26 21:26:46 +0900

    [Feat]: Faster random puzzle selection query

--- storage/storage.go
+++ storage/storage.go
@@ -27,15 +27,26 @@ func InitDB(username, password, host, port, dbname string) error {
 
 func FetchPuzzle() (types.Puzzle, error) {
 	row := db.QueryRow(`
-    SELECT p.id, p.row_size, p.col_size, p.clues,
+        WITH selected_puzzle AS (
+        SELECT id, row_size, col_size, clues
+        FROM puzzles TABLESAMPLE BERNOULLI(1)
+        ORDER BY RANDOM()
+        LIMIT 1
+    )
+
+    SELECT 
+        p.id, p.row_size, p.col_size, p.clues,
         COALESCE(pt.title, 'Untitled') AS title,
-        COALESCE(a.name, 'Anonymous') AS author
-    FROM puzzles p
-    LEFT JOIN puzzle_titles pt ON p.id = pt.puzzle_id AND pt.language = 'ko'
-    LEFT JOIN puzzle_metadata pm ON p.id = pm.puzzle_id 
-    LEFT JOIN authors a ON pm.author_id = a.id
-    ORDER BY RANDOM()
-    LIMIT 1`)
+        COALESCE(a.name, 'anonymous') AS author
+    FROM selected_puzzle p 
+    LEFT JOIN 
+        puzzle_titles pt ON p.id = pt.puzzle_id AND pt.language = 'ko' 
+    LEFT JOIN
+        puzzle_metadata pm ON p.id = pm.puzzle_id
+    LEFT JOIN
+        authors a ON pm.author_id = a.id
+    `)
+
 	var puzzle types.Puzzle
 	err := row.Scan(&puzzle.ID, &puzzle.RowSize, &puzzle.ColSize, &puzzle.Clues,
 		&puzzle.Title, &puzzle.Author)
